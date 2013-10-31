adc007f31c0d71567b0f69d56575f2295f0a97dc
Yannick Brosseau <yannick.brosseau@gmail.com>
2013-10-31 18:10:29 -0400

    Change licence to GPLv2 for files moved from the old ltt directory to lttv
    Signed-off-by: Yannick Brosseau <yannick.brosseau@gmail.com>

--- lttv/lttv/time.h
+++ lttv/lttv/time.h
@@ -2,19 +2,19 @@
  * Copyright (C) 2003-2004 Michel Dagenais
  *               2005 Mathieu Desnoyers
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License Version 2.1 as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License Version 2 as
+ * published by the Free Software Foundation;
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, 
+ * MA 02110-1301, USA
  */
 
 #ifndef LTT_TIME_H
